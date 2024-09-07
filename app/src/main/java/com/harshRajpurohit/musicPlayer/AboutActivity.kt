bbf8f5fb899a1f381dcaead077a148a38eea273c
xuhuabao <ranzi3096459788@gmail.com>
2024-09-07 21:08:53 +0800

    侧滑导航栏UI优化，及activity逻辑修改

--- app/src/main/java/com/harshRajpurohit/musicPlayer/AboutActivity.kt
+++ app/src/main/java/com/harshRajpurohit/musicPlayer/AboutActivity.kt
@@ -17,7 +17,7 @@ class AboutActivity : AppCompatActivity() {
         binding.aboutText.text = aboutText()
     }
     private fun aboutText(): String{
-        return "Developed By: Harsh H. Rajpurohit" +
+        return "Developed By: Xu Huabao" +
                 "\n\nIf you want to provide feedback, I will love to hear that."
     }
 }
